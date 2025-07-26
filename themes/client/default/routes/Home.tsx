23d09880207a944d76ab1b72bade46175351d0bb
Devin G <149845346+DeathSoulYTDev@users.noreply.github.com>
2025-07-26 14:25:11 -0400

    Create Home.tsx

---
+++ themes/client/default/routes/Home.tsx
@@ -0,0 +1,10 @@
+import Welcome from "../components/Welcome";
+
+export default function Home() {
+    return (
+        <div className="w-full h-full">
+            <Welcome /> to the Home Page<br />
+            <span>The Contents of this Page Can be Customized in the <b>/themes/admin/default/routes/Home.tsx</b> Files</span>
+        </div>
+    )
+}
